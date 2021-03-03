36a1093cc6a21c414471675f68697609a5bca528
Edwin Samodra <edwinsp001@gmail.com>
2021-03-03 21:27:43 +0700

    mencoba fix bug deploy heroku

--- models/index.js
+++ models/index.js
@@ -1,37 +1,27 @@
-'use strict';
+const { Sequelize } = require('sequelize');
+const { database } = require('../config/config')
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/index.js')[env];
-const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(database.sequelize.database, database.sequelize.username, database.sequelize.password, {
+  host: database.sequelize.host,
+  dialect: database.sequelize.dialect,
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+  pool: {
+      max: 3,
+      min: 0,
+      idle: 3000,
+      acquire: 10000
+  }   
+})
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const users = require("./user")(sequelize, Sequelize);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = {
+  Sequelize,
+  sequelize,
+  
+  users
+}
